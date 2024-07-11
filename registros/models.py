d627891aaac72150dc9050c9b4665ed06b8f04b7
sanjaygrr <sanjay.ramchandani@triciclos.cl>
2024-07-11 06:18:10 -0400

    crear models

---
+++ registros/models.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import Group
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Trabajo(models.Model):
+    fecha = models.DateField()
+    faena = models.CharField(max_length=100)
+    maquina = models.CharField(max_length=100)
+    trabajo = models.CharField(max_length=200)
+    horometro_inicial = models.IntegerField()
+    horometro_final = models.IntegerField()
+    total_horas = models.IntegerField()
+    petroleo_litros = models.FloatField()
+    aceite_tipo_litros = models.CharField(max_length=200)
+    observaciones = models.TextField()
+    supervisor = models.ForeignKey(
+        User, related_name='supervisores', on_delete=models.CASCADE)
+    trabajador = models.ForeignKey(
+        User, related_name='trabajadores', on_delete=models.CASCADE)
+    aprobado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.faena} - {self.maquina} - {self.fecha}"
+
+
+@receiver(post_save, sender=User)
+def add_user_to_default_group(sender, instance, created, **kwargs):
+    if created:
+        group = Group.objects.get(name='Trabajador')
+        instance.groups.add(group)
