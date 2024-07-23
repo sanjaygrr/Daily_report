62e62f1870e04a62261b44b265fbfb131a13e525
sanjaygrr <sanjay.ramchandani@triciclos.cl>
2024-07-23 10:32:16 -0400

    modificar mdoels

--- registros/models.py
+++ registros/models.py
@@ -4,18 +4,30 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
+class Maquina(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Faena(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+
 class Trabajo(models.Model):
     fecha = models.DateField()
-    faena = models.CharField(max_length=100)
-    maquina = models.CharField(max_length=100)
+    faena = models.ForeignKey(Faena, on_delete=models.CASCADE)
+    maquina = models.ForeignKey(Maquina, on_delete=models.CASCADE)
     trabajo = models.CharField(max_length=200)
-    horometro_inicial = models.DecimalField(
-        max_digits=10, decimal_places=2)  # Permite hasta 99999999.99
-    horometro_final = models.DecimalField(
-        max_digits=10, decimal_places=2)  # Permite hasta 99999999.99
+    horometro_inicial = models.DecimalField(max_digits=10, decimal_places=2)
+    horometro_final = models.DecimalField(max_digits=10, decimal_places=2)
     total_horas = models.DecimalField(
-        max_digits=12, decimal_places=2, blank=True, null=True)  # Permite hasta 9999999999.99
-    petroleo_litros = models.FloatField()
+        max_digits=12, decimal_places=2, null=True, blank=True)
+    petroleo_litros = models.DecimalField(max_digits=10, decimal_places=2)
     aceite_tipo_litros = models.CharField(max_length=200)
     observaciones = models.TextField()
     supervisor = models.ForeignKey(
@@ -23,6 +35,8 @@ class Trabajo(models.Model):
     trabajador = models.ForeignKey(
         User, related_name='trabajadores', on_delete=models.CASCADE)
     aprobado = models.BooleanField(default=False)
+    tipo_medida = models.CharField(max_length=10, choices=[(
+        'Horas', 'Horas'), ('Kilómetros', 'Kilómetros')], default='Horas')
 
     def __str__(self):
         return f"{self.faena} - {self.maquina} - {self.fecha}"
