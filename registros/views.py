54040061312ae9c4a6a6f44843cd6dd184a35e13
sanjaygrr <sanjay.ramchandani@triciclos.cl>
2024-07-23 10:32:27 -0400

    modificar views

--- registros/views.py
+++ registros/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from .models import Trabajo
-from .forms import TrabajoForm, UserRegistrationForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required, user_passes_test
+from .models import Trabajo, Maquina, Faena
+from .forms import TrabajoForm, MaquinaForm, FaenaForm, UserRegistrationForm
 from .filters import TrabajoFilter
 from django.utils import timezone
 from django.http import HttpResponse
 import openpyxl
 from openpyxl.utils import get_column_letter
 from django.contrib.auth.models import User
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+import os
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.conf import settings
+from reportlab.lib.units import inch
+from reportlab.lib import colors
 
 
 def home(request):
@@ -36,18 +44,19 @@ def historial(request):
 
 
 @login_required
+@user_passes_test(lambda u: u.groups.filter(name='Supervisor').exists() or u.is_superuser or u.groups.filter(name='Admin').exists())
+def pendientes(request):
+    trabajos = Trabajo.objects.filter(supervisor=request.user, aprobado=False)
+    return render(request, 'registros/pendientes.html', {'trabajos': trabajos})
+
+
+@login_required
 def aprobar_trabajo(request, pk):
     trabajo = get_object_or_404(Trabajo, pk=pk)
     if request.user == trabajo.supervisor:
         trabajo.aprobado = True
         trabajo.save()
     return redirect('pendientes')
-
-
-@login_required
-def pendientes(request):
-    trabajos = Trabajo.objects.filter(supervisor=request.user, aprobado=False)
-    return render(request, 'registros/pendientes.html', {'trabajos': trabajos})
 
 
 @login_required
@@ -79,14 +88,14 @@ def export_historial_xlsx(request):
         row_num += 1
         row = [
             trabajo.fecha,
-            trabajo.faena,
-            trabajo.maquina,
+            trabajo.faena.nombre,  # Convertir instancia a nombre
+            trabajo.maquina.nombre,  # Convertir instancia a nombre
             trabajo.trabajo,
             trabajo.horometro_inicial,
             trabajo.horometro_final,
             trabajo.total_horas,
-            getattr(trabajo, 'petroleo', ''),
-            getattr(trabajo, 'aceite', ''),
+            trabajo.petroleo_litros,
+            trabajo.aceite_tipo_litros,
             trabajo.observaciones,
             trabajo.supervisor.username if trabajo.supervisor else '',
             trabajo.trabajador.username if trabajo.trabajador else '',
@@ -113,9 +122,127 @@ def register_user(request):
 
 
 @login_required
+@user_passes_test(lambda u: u.is_superuser or u.groups.filter(name='Admin').exists())
 def listar_usuarios(request):
-    if request.user.is_superuser:  # Solo los superusuarios pueden ver esta lista
-        usuarios = User.objects.all()
-        return render(request, 'registros/listar_usuarios.html', {'usuarios': usuarios})
+    usuarios = User.objects.all()
+    return render(request, 'registros/listar_usuarios.html', {'usuarios': usuarios})
+
+
+@login_required
+def crear_maquina(request):
+    if request.method == 'POST':
+        form = MaquinaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_maquinas')
     else:
-        return redirect('home')
+        form = MaquinaForm()
+    return render(request, 'registros/crear_maquina.html', {'form': form})
+
+
+@login_required
+def listar_maquinas(request):
+    maquinas = Maquina.objects.all()
+    return render(request, 'registros/listar_maquinas.html', {'maquinas': maquinas})
+
+
+@login_required
+def crear_faena(request):
+    if request.method == 'POST':
+        form = FaenaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_faenas')
+    else:
+        form = FaenaForm()
+    return render(request, 'registros/crear_faena.html', {'form': form})
+
+
+@login_required
+def listar_faenas(request):
+    faenas = Faena.objects.all()
+    return render(request, 'registros/listar_faenas.html', {'faenas': faenas})
+
+
+@login_required
+def generar_pdf_trabajo(request, pk):
+    trabajo = get_object_or_404(Trabajo, pk=pk)
+
+    if request.method == 'POST' and request.FILES.get('logo'):
+        logo = request.FILES['logo']
+        logo_path = default_storage.save(
+            f'logos/{logo.name}', ContentFile(logo.read()))
+
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = f'attachment; filename=Trabajo_{trabajo.fecha}.pdf'
+
+        p = canvas.Canvas(response, pagesize=letter)
+        width, height = letter
+
+        # Añadir el logo con más margen desde la parte superior
+        if logo_path:
+            p.drawImage(os.path.join(settings.MEDIA_ROOT, logo_path), width -
+                        2 * inch, height - inch, width=1 * inch, height=0.75 * inch)
+
+        # Título con más espacio desde la parte superior
+        p.setFont("Helvetica-Bold", 16)
+        p.drawString(inch, height - 1.8 * inch, "Reporte de Trabajo")
+
+        # Líneas de separación
+        p.setStrokeColor(colors.grey)
+        p.setLineWidth(0.5)
+        p.line(inch, height - 1.85 * inch, width - inch, height - 1.85 * inch)
+
+        # Datos del trabajo
+        p.setFont("Helvetica", 12)
+        y = height - 2.5 * inch
+        left_column_x = inch + 10
+        right_column_x = width / 2 + 10
+        line_height = 16
+
+        detalles = [
+            ("Fecha", trabajo.fecha),
+            ("Faena", trabajo.faena.nombre),
+            ("Máquina", trabajo.maquina.nombre),
+            ("Trabajo", trabajo.trabajo),
+            ("Tipo de Medida", trabajo.tipo_medida),
+            ("Horómetro Inicial", trabajo.horometro_inicial),
+            ("Horómetro Final", trabajo.horometro_final),
+            ("Total de Horas", trabajo.total_horas),
+            ("Petróleo (litros)", trabajo.petroleo_litros),
+            ("Aceite (tipo y litros)", trabajo.aceite_tipo_litros),
+            ("Observaciones", trabajo.observaciones),
+            ("Supervisor", trabajo.supervisor.username),
+            ("Trabajador", trabajo.trabajador.username)
+        ]
+
+        for i, (label, value) in enumerate(detalles):
+            if i % 2 == 0:
+                p.drawString(left_column_x, y, f"{label}: {value}")
+            else:
+                p.drawString(right_column_x, y, f"{label}: {value}")
+                y -= line_height
+            if label == "Observaciones":
+                y -= line_height  # Extra espacio después de observaciones
+
+        p.showPage()
+        p.save()
+
+        return response
+    else:
+        return redirect('historial')
+
+
+@login_required
+def upload_logo(request):
+    if request.method == 'POST':
+        form = LogoUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            logo = form.cleaned_data['logo']
+            fs = FileSystemStorage()
+            filename = fs.save(logo.name, logo)
+            request.session['logo_path'] = fs.url(filename)
+            return redirect('home')
+    else:
+        form = LogoUploadForm()
+    return render(request, 'registros/upload_logo.html', {'form': form})
