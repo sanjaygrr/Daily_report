27e76da187c2fa64e42f850448e843fb1f5b3f56
sanjaygrr <sanjay.ramchandani@triciclos.cl>
2024-07-28 17:35:29 -0400

    arreglar tamaño de navbar y de tabla de historial

--- registros/filters.py
+++ registros/filters.py
@@ -1,9 +1,14 @@
 import django_filters
 from .models import Trabajo
+from django.contrib.auth.models import User
 
 
 class TrabajoFilter(django_filters.FilterSet):
+    supervisor = django_filters.ModelChoiceFilter(
+        queryset=User.objects.filter(groups__name='Supervisor')
+    )
+
     class Meta:
         model = Trabajo
-        fields = ['fecha', 'faena', 'maquina', 'trabajo',
-                  'supervisor', 'trabajador', 'aprobado']
+        fields = ['fecha', 'faena', 'maquina',
+                  'trabajador', 'supervisor', 'aprobado']
