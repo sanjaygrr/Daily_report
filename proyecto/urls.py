e21b3019d09693a183327e5d0590b9a00fa4bc7e
sanjaygrr <sanjaygr@gmail.com>
2025-05-29 10:36:24 -0400

    arreglar envio de correos

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from registros import views as registros_views
+from registros.forms import CustomPasswordResetForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,7 +11,7 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/',
          auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html', form_class=CustomPasswordResetForm), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
