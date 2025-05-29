a65396f116af076307d24f8a360500684215c1f3
sanjaygrr <sanjaygr@gmail.com>
2025-05-29 09:58:32 -0400

    agregar recueorar cintraseña

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -147,3 +147,12 @@ AUTHENTICATION_BACKENDS = (
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
+
+# Configuración de correo para recuperación de contraseña
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'TU_CORREO@gmail.com'  # Cambia esto por tu correo real
+EMAIL_HOST_PASSWORD = 'TU_CONTRASEÑA_DE_APLICACION'  # Cambia esto por tu contraseña de aplicación
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
