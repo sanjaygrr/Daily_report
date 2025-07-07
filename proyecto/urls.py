55b6d6517beea6674bca3f10a7cc0d0f4d9c842f
Sanjay Ramchandani <sanjay@hotmail.es>
2025-07-07 10:48:49 -0400

    formatear rut en inicio de sesion

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from registros import views as registros_views
 from registros.forms import CustomPasswordResetForm
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,3 +18,8 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Opcional: también servir archivos estáticos desde STATIC_ROOT en desarrollo
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
